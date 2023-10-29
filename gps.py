da20f14454bd92f21e9b1aeb221de223b517abf8
Chris Binggeli <99316773+GNUNotUsername@users.noreply.github.com>
2023-10-29 13:19:55 +1000

    Database creation/connection

--- gps.py
+++ gps.py
@@ -1,14 +1,15 @@
 """
 A web spider for analysing GPS data accidentally committed to github
 
-USAGE:  python gps.py count [repo-url]
+USAGE:  sudo python gps.py count [repo-url]
 """
 
 
 from os     import path
 from sys    import argv
 
-from sqlalchemy import create_engine
+from sqlalchemy         import create_engine
+from sqlalchemy_utils   import create_database, database_exists
 
 import re
 
@@ -19,6 +20,9 @@ GOOD_ARGVS  = (2, 3)
 URL         = 2
 USE_URL     = 3
 
+# Databases
+DB_ADDR = "mariadb://root@localhost:3306/gitprivacyspider"
+
 # Exit codes
 BAD_ARGV    = 1
 
@@ -27,6 +31,12 @@ WRITE   = "w"
 
 # Regex
 GH_REG  = r"https://github\.com/[A-Za-z]+/([A-Za-z0-9]+(\.[A-Za-z0-9]+)+)"
+
+
+def connect_db():
+    engine = create_engine(DB_ADDR)
+    if not database_exists(engine.url):
+        create_database(engine.url)
 
 
 def validate(argv):
@@ -49,6 +59,8 @@ def validate(argv):
 def main():
     if not validate(argv):
         exit(BAD_ARGV)
+
+    connect_db()
 
 
 if __name__ == "__main__":
